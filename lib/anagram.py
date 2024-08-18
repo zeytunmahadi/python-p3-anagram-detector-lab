c85faa90db8d5259460ea8defd4d181c01d4f30a
zeitun <zeytunmahadi718@gmail.com>
2024-08-18 15:07:34 +0300

    avoiding repeatation of code

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = ''.join(sorted(word))
+    
+    def match(self, possible_anagrams):
+        return [word for word in possible_anagrams if ''.join(sorted(word)) == self.sorted_word]
+
+
+if __name__ == "__main__":
+    anagram = Anagram('listen')
+    possible_anagrams = ['enlists', 'google', 'inlets', 'banana']
+    print(anagram.match(possible_anagrams))  
